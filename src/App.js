8a125276dd81747fea74237debabb4a767df44f9
Sonia Yadav <sonia@sayurbox.com>
2020-09-26 15:35:34 +0530

    test change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends React.Component{
     render() {
         return(
             <div>
-                <h1>App Covid</h1>
+                <h1>App Covid Start</h1>
             </div>
         )
     }
